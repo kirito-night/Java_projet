bdea1e0d3bd5e9b49239244eb9400dc1c8c799bd
Junji <qq870185814@163.com>
2020-11-17 20:22:07 +0100

    creation of class stimulation

--- Stimulation.java
+++ Stimulation.java
@@ -1,3 +1,14 @@
+import java.util.ArrayList;
 public class Stimulation {
-    private 
+    private Terrain terrain;
+    private ArrayList<Ressource> ressources;
+    private ArrayList<Agent> agents;
+
+    public Stimulation(Terrain terrain, ArrayList<Ressource> ressources, ArrayList<Agent> agents) {
+        this.terrain = terrain;
+        this.ressources = ressources;
+        this.agents = agents;
+    }
+
+    
 }
