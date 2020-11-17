66a4cd835850620cb583734e1187a3f177556bb1
Junji <qq870185814@163.com>
2020-11-17 20:47:43 +0100

    stimulation

--- Stimulation.java
+++ Stimulation.java
@@ -4,15 +4,27 @@ public class Stimulation {
     private ArrayList<Ressource> ressources;
     private ArrayList<Agent> agents;
 
-    public Stimulation(int lig, int col, int n, int m) {
+    public Stimulation(int lig, int col, int m, int n) {
         this.terrain = new Terrain(lig, col);
 
-        ressources = new ArrayList<Ressource>(n);
-        for(int i = 0; i < n; i++){
-
+        ressources = new ArrayList<Ressource>();
+        for(int i = 0; i < m/2; i++){
+            ressources.add(new Ressource("base", 1));
+            ressources.add(new Ressource("bomb", 1));
+        }
+        if(m % 2 == 1){
+            ressources.add(new Ressource("base", 1));
         }
 
-        this.ressources = ressources;
+
+        agents = new ArrayList<Agent>();
+        for(int i = 0; i < n/2; i++){
+            agents.add(new Police(Math.random()*));
+            agents.add(new Ressource("bomb", 1));
+        }
+        if(n % 2 == 1){
+            agents.add(new Ressource("base", 1));
+        }
         this.agents = agents;
     }
 
