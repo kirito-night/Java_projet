bfd6f5638b23d80d3e0c551bc2ef5a9a6125b1de
Junji <qq870185814@163.com>
2020-11-21 16:25:23 +0100

    affiche.terrain

--- Simulation.java
+++ Simulation.java
@@ -83,6 +83,7 @@ public class Simulation {
                     terrain.setCase(x, y, ress);
                     System.out.println(ress.toString());
                     Outils.ressourceRecord(ress, 1);
+                    terrain.affiche();
                 }
             } else {// sinon, voir le type de ressource
                 ress = terrain.getCase(x, y);
@@ -114,6 +115,7 @@ public class Simulation {
                         }
                         ress.initialisePosition();
                         ressources.remove(ress);
+                        terrain.affiche();
                     }
                 }
             }
@@ -121,7 +123,6 @@ public class Simulation {
             if(Outils.avoirLieu(terrain.nbLigne*terrain.nbColonnes/(agents.size()/2.)/100)){
                 agent.seBattre();
             }
-            terrain.affiche();
         }
     }
     public void moveAgents(){ // deplacer tous les agents
