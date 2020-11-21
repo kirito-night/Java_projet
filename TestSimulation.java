c8561c10ef93f84357e6261ba6e134e59c22e415
kirito-night <zengfx4869@gmail.com>
2020-11-21 16:08:42 +0100

    tests

--- TestSimulation.java
+++ TestSimulation.java
@@ -1,6 +1,6 @@
 public class TestSimulation {
     public static void main(String[] args) {
-        Simulation s1 = new Simulation(10, 10 , 100, 20);
+        Simulation s1 = new Simulation(4, 5 , 100, 20);
         for (int i = 0; i < 10; i++) {
             s1.agentsAction();
             s1.moveAgents();
